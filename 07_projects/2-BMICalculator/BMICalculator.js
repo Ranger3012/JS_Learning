bbd8c11d80a3e8170d677a6c0bd766bee2ff6cc1
Ranger3012 <78731159+Ranger3012@users.noreply.github.com>
2024-10-20 18:39:26 -0400

    changes

--- 07_projects/2-BMICalculator/BMICalculator.js
+++ 07_projects/2-BMICalculator/BMICalculator.js
@@ -16,6 +16,12 @@ form.addEventListener('submit', function(e){
     else{
         const bmi = (weight / ((height * height)/10000)).toFixed(2)
 
-        results.innerHTML = `<span>${bmi}</span>`;
+        if(bmi < 18.6){
+            results.innerHTML = `<span>${bmi}, you are under weight !</span>`;
+        }else if(bmi > 18.6 && bmi < 24.9){
+            results.innerHTML = `<span>${bmi}, you are in normal range !</span>`;
+        }else {
+            results.innerHTML = `<span>${bmi}, you are overweight !</span>`;
+        }
     }
 });
